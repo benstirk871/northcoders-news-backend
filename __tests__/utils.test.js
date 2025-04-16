cc7069f3188d1dc6cd2e89c708febdb7c22f5245
Your Name <you@example.com>
2025-04-16 11:32:52 +0100

    Created a  function to retrieve article_id

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,6 +1,7 @@
 const {
-  convertTimestampToDate
+  convertTimestampToDate, createRef
 } = require("../db/seeds/utils");
+
 
 describe("convertTimestampToDate", () => {
   test("returns a new object", () => {
@@ -38,3 +39,82 @@ describe("convertTimestampToDate", () => {
   });
 });
 
+describe("createRef", () =>{
+  test("Returns empty object when passed an empty array", () =>{
+    const input =[]
+    const expectedOutput = {}
+
+    const actualOutput = createRef(input) 
+
+    expect(actualOutput).toEqual(expectedOutput)
+  })
+  test("Returns a reference object with a single key/ value pair when passed an array with a single object", () =>{
+    const input =[{
+      article_id: 1,
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: '2020-07-09T20:11:00.000Z',
+      votes: 100,
+      article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+    }]
+    const expectedOutput = {'Living in the shadow of a great man': 1}
+
+    const actualOutput = createRef(input) 
+
+    expect(actualOutput).toEqual(expectedOutput)
+  })
+  test("Returns a reference object with mutliple key/ value pairs when passed an array with multiple objects", () =>{
+    const input =[ {
+      article_id: 1,
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: '2020-07-09T20:11:00.000Z',
+      votes: 100,
+      article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+    },
+    {
+      article_id: 2,
+      title: 'Sony Vaio; or, The Laptop',
+      topic: 'mitch',
+      author: 'icellusedkars',
+      body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+      created_at: '2020-10-16T05:03:00.000Z',
+      votes: 0,
+      article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+    },
+    {
+      article_id: 3,
+      title: 'Eight pug gifs that remind me of mitch',
+      topic: 'mitch',
+      author: 'icellusedkars',
+      body: 'some gifs',
+      created_at: '2020-11-03T09:12:00.000Z',
+      votes: 0,
+      article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+    },
+    {
+      article_id: 4,
+      title: 'Student SUES Mitch!',
+      topic: 'mitch',
+      author: 'rogersop',
+      body: 'We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages',
+      created_at: '2020-05-06T01:14:00.000Z',
+      votes: 0,
+      article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+    },]
+    const expectedOutput = {
+      'Living in the shadow of a great man': 1,
+      'Sony Vaio; or, The Laptop': 2,
+      'Eight pug gifs that remind me of mitch': 3,
+      'Student SUES Mitch!': 4
+    }
+
+    const actualOutput = createRef(input) 
+
+    expect(actualOutput).toEqual(expectedOutput)
+  })
+})
