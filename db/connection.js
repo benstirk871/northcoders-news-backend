744dda67864f707b62ae7d97df52518540ca46d4
Your Name <you@example.com>
2025-05-01 10:50:23 +0100

    Supabase set up

--- db/connection.js
+++ db/connection.js
@@ -6,17 +6,19 @@ const config = {};
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
-const db = new Pool(config);
-
 if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
 }
+
+const db = new Pool(config);
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("No PGDATABASE configured or DATABASE_URL not set")
-} else { 
+} else if (process.env.PGDATABASE){ 
     console.log(`Connected to ${process.env.PGDATABASE}`)
+} else if (process.env.DATABASE_URL) {
+    console.log(`Connected to ${process.env.DATABASE_URL}`)
 }
 
 
